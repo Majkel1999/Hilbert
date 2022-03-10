61b894037dd99fc9c421164efc973c1a517ce7fd
Michał Kaleta <mkaleta1999@gmail.com>
2022-03-10 16:22:38 +0100

    fixed launching with and without docker automatically

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,12 +1,18 @@
+import os
+
 from beanie import init_beanie
 from motor.motor_asyncio import AsyncIOMotorClient
-from app.models.project_models import Project
 
+from app.models.project_models import Project
 from app.models.user_models import User
 
-DB_CONN_STRING = "mongodb://root:root@localhost:8001"
+DB_CONN_STRING = os.environ.get('DB_CONN_STRING', False)
+
+if DB_CONN_STRING is False:
+    DB_CONN_STRING = "mongodb://root:root@localhost:8001"
+
 
 async def init_db():
     print("Initializing database connection")
     client = AsyncIOMotorClient(DB_CONN_STRING)
-    await init_beanie(client.test, document_models=[User,Project])
+    await init_beanie(client.test, document_models=[User, Project])
