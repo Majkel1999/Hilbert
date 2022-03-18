10eaa2fe6ea3c098e7e88be990d231dc252b0c9e
Kacper <kacper.791@o2.pl>
2022-03-18 22:25:23 +0100

    fixes

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,14 +2,18 @@ import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import ReactDOM from 'react-dom';
 import './index.css';
+import { Provider } from 'react-redux';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import store from './store/index';
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root'),
 );
