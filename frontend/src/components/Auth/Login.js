648baf2925798bc20cdfbde2fb7215c4caac2c44
Kacper <kacper.791@o2.pl>
2022-03-23 22:00:43 +0100

    stop using service file

--- frontend/src/components/Auth/Login.js
+++ frontend/src/components/Auth/Login.js
@@ -1,10 +1,20 @@
 import React from 'react';
-import * as authService from './AuthService';
+import axios from '../../api/axios';
+import { authActions } from '../../store/Slices/auth';
 
 export default function Login() {
-  const login = () => {
-    authService.login('test1', 'test2');
+  const login = async (username, password) => {
+    try {
+      const response = await axios.post('/user/login', {
+        username,
+        password,
+      });
+      console.log(response);
+    } catch (error) {
+      console.log(error);
+    }
   };
+
   return (
     <button type="button" onClick={login}>
       Click
--- frontend/src/components/Auth/Register.js
+++ frontend/src/components/Auth/Register.js
@@ -1,5 +1,19 @@
 import React from 'react';
+import axios from '../../api/axios';
+import { authActions } from '../../store/Slices/auth';
 
 export default function Register() {
+  const register = async (username, password) => {
+    try {
+      const response = await axios.post('user/register', {
+        username,
+        password,
+      });
+      store.dispatch(authActions.login());
+      console.log(response);
+    } catch (error) {
+      console.log(error);
+    }
+  };
   return <div>Register</div>;
 }
