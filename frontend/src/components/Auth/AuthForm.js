c2eff0315666d5a1201e05a277a57c48d2084a9e
Kacper <kacper.791@o2.pl>
2022-03-24 21:29:49 +0100

    formating code to be Generic

--- frontend/src/components/Auth/AuthForm.js
+++
@@ -1,3 +0,0 @@
-export default function AuthForm() {
-  return <div>authForm</div>;
-}
--- frontend/src/components/GenericForm.js
+++ frontend/src/components/GenericForm.js
@@ -1,15 +1,45 @@
 import PropTypes from 'prop-types';
+import { capitalizeFirstLetter } from '../utils/utils';
 
-export default function GenericForm({ header }) {
+export default function GenericForm({
+  header,
+  onSubmitHandler,
+  formInputArray,
+  buttonText,
+}) {
   return (
     <div className="formContainer">
       <h1 className="formName">{header}</h1>
+      <form onSubmit={onSubmitHandler}>
+        {formInputArray.map((item) => (
+          <div className="inputContainer">
+            <label htmlFor={item.label}>
+              {capitalizeFirstLetter(item.label)}:
+            </label>
+            <input
+              type={item.inputType}
+              id={item.label}
+              autoComplete="off"
+              onChange={(e) => item.setValue(e.target.value)}
+              value={item.inputValue}
+              required
+            />
+          </div>
+        ))}
+        <button type="submit">{buttonText}</button>
+      </form>
     </div>
   );
 }
 GenericForm.propTypes = {
   header: PropTypes.string,
+  buttonText: PropTypes.string,
+  onSubmitHandler: PropTypes.func,
+  formInputArray: PropTypes.arrayOf,
 };
 GenericForm.defaultProps = {
   header: '',
+  buttonText: '',
+  onSubmitHandler: () => {},
+  formInputArray: [],
 };
