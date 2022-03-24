2e9cdfc31e4aec1c1acf63b43192be7729b1b598
Michał Kaleta <mkaleta1999@gmail.com>
2022-03-24 08:34:08 +0100

    Merge remote-tracking branch 'origin/kkol/typing'

--- frontend/src/components/Auth/Login.js
+++ frontend/src/components/Auth/Login.js
@@ -1,13 +1,61 @@
-import React from 'react';
-import * as authService from './AuthService';
+import React, { useState } from 'react';
+
+import { useDispatch } from 'react-redux';
+import { useNavigate, useLocation } from 'react-router-dom';
+import axios from '../../api/axios';
+import { authActions } from '../../store/Slices/auth';
+
+const LOGIN_URL = '/user/login';
 
 export default function Login() {
-  const login = () => {
-    authService.login();
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const location = useLocation();
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const previousPage = location?.state?.from?.pathname || '/';
+
+  const login = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post(LOGIN_URL, {
+        username,
+        password,
+      });
+      dispatch(authActions.login());
+      navigate(previousPage, { replace: true });
+
+      console.log(response);
+    } catch (error) {
+      console.log(error);
+    }
   };
+
   return (
-    <button type="button" onClick={login}>
-      Click
-    </button>
+    <>
+      <h1>Login</h1>
+      <form onSubmit={login}>
+        <label htmlFor="username">Username: </label>
+        <input
+          type="text"
+          id="username"
+          autoComplete="off"
+          onChange={(e) => setUsername(e.target.value)}
+          value={username}
+          required
+        />
+        <label htmlFor="password">Password: </label>
+        <input
+          type="password"
+          id="password"
+          autoComplete="off"
+          onChange={(e) => setPassword(e.target.value)}
+          value={password}
+          required
+        />
+        <button type="button">Login</button>
+      </form>
+    </>
   );
 }
