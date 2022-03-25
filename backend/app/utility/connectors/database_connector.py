963edc26aa70f930fee0f7b7f662abee30370f42
Michał Kaleta <mkaleta1999@gmail.com>
2022-03-25 11:25:42 +0100

    Added proper logging and message consumer

--- backend/app/utility/connectors/database_connector.py
+++ backend/app/utility/connectors/database_connector.py
@@ -6,8 +6,10 @@ from motor.motor_asyncio import AsyncIOMotorClient
 from app.models.project_models import MLModel, Project, TextDocument
 from app.models.user_models import User
 
+
 class Database:
     client: AsyncIOMotorClient = None
+
 
 DB_CONN_STRING = os.environ.get('DB_CONN_STRING', False)
 
@@ -16,10 +18,13 @@ if DB_CONN_STRING is False:
 
 db = Database()
 
+
 async def init_db():
-    print("Initializing database connection")
+    print("Initializing database connection", flush=True)
     db.client = AsyncIOMotorClient(DB_CONN_STRING)
     await init_beanie(db.client.test, document_models=[User, Project, TextDocument, MLModel])
+    print("Database connection initialized", flush=True)
+
 
 async def close_db():
     db.client.close()
--- mlService/connectors/rabbitmq_connector.py
+++ mlService/connectors/rabbitmq_connector.py
@@ -1,4 +1,5 @@
 import asyncio
+from typing import Callable
 import aio_pika
 
 RABBIT_HOST = "rabbitmq"
@@ -10,18 +11,19 @@ class RabbitMQHandler:
     def __init__(self):
         self._connection = None
         self._channel = None
+        self._queue = None
 
     async def init(self):
-        print("Initializing RabbitMQ connection", flush=True)
+        print("Initializing RabbitMQ connection")
         while(True):
             try:
                 self._connection = await aio_pika.connect_robust(host=RABBIT_HOST)
                 self._channel = await self._connection.channel()
-                await self._channel.declare_queue(QUEUE_NAME)
-                print("RabbitMQ connection initialized", flush=True)
+                self._queue = await self._channel.declare_queue(QUEUE_NAME)
+                print("RabbitMQ connection initialized")
                 return
             except:
-                print('RabbitMQ connection failed. Retrying in 5s...', flush=True)
+                print('RabbitMQ connection failed. Retrying in 5s...')
                 await asyncio.sleep(5)
 
     async def sendMessage(self, body: str, queue_name: str = QUEUE_NAME):
@@ -29,6 +31,13 @@ class RabbitMQHandler:
             aio_pika.Message(body.encode()),
             routing_key=queue_name
         )
+
+    async def consumeMessages(self, process_message : Callable[[aio_pika.abc.AbstractIncomingMessage],None] ):
+        await self._queue.consume(process_message)
+        try:
+            await asyncio.Future()
+        finally:
+            await self.closeConnection()
 
     async def closeConnection(self):
         await self._connection.close()
