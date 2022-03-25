082247c697e5cd1ad912172e83734fd409e158ee
Kacper <kacper.791@o2.pl>
2022-03-25 22:51:47 +0100

    creating input component, styling components

--- frontend/src/components/GenericForm/GenericForm.js
+++ frontend/src/components/GenericForm/GenericForm.js
@@ -1,6 +1,7 @@
 import PropTypes from 'prop-types';
-import { capitalizeFirstLetter } from '../../utils/utils';
-import Button from '../UI/Button';
+import Button from '../UI/Button/Button';
+import Input from '../UI/Input/Input';
+import './GenericForm.scss';
 
 export default function GenericForm({
   header,
@@ -11,21 +12,16 @@ export default function GenericForm({
   return (
     <div className="formContainer">
       <h1 className="formName">{header}</h1>
-      <form onSubmit={onSubmitHandler}>
+      <form className="form" onSubmit={onSubmitHandler}>
         {formInputArray.map((item) => (
-          <div className="inputContainer" key={item.label}>
-            <label htmlFor={item.label}>
-              {capitalizeFirstLetter(item.label)}:
-            </label>
-            <input
-              type={item.inputType}
-              id={item.label}
-              autoComplete="off"
-              onChange={(e) => item.setValue(e.target.value)}
-              value={item.inputValue}
-              required
-            />
-          </div>
+          <Input
+            key={item.label}
+            labelName={item.label}
+            type={item.inputType}
+            id={item.label}
+            onChange={(e) => item.setValue(e.target.value)}
+            value={item.inputValue}
+          />
         ))}
         <Button type="submit">{buttonText}</Button>
       </form>
