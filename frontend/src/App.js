507b848b92881fa4e9d881b84a47ec31fda9e649
Kacper <kacper.791@o2.pl>
2022-03-27 23:01:38 +0200

    displaying pages depending on login status

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,16 +4,20 @@ import Login from './components/Auth/Login';
 import Register from './components/Auth/Register';
 import Layout from './components/Layout/Layout';
 import RequireAuth from './components/Auth/RequireAuth';
+import AdminBoard from './pages/AdminBoard/AdminBoard';
+import Home from './pages/Home/Home';
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
         {/* Available for everyone */}
+        <Route path="/" element={<Home />} />
         <Route path="login" element={<Login />} />
         <Route path="register" element={<Register />} />
         {/* protected routes */}
         <Route element={<RequireAuth />}>
+          <Route path="/" element={<AdminBoard />} />
           {/* <Route path="protectedRoute" element={<protectedComponent />} /> */}
         </Route>
         {/* if none of routes above --> create notFound page */}
---
+++ frontend/src/pages/AdminBoard/AdminBoard.js
@@ -0,0 +1,3 @@
+export default function AdminBoard() {
+  return <div>AdminBoard</div>;
+}
