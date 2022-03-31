3f24f690f0069cbbf9f37b5c002b010672a721b3
Kacper <kacper.791@o2.pl>
2022-04-01 00:15:26 +0200

    styling table

--- frontend/src/components/Project/ProjectItem/ProjectItem.js
+++ frontend/src/components/Project/ProjectItem/ProjectItem.js
@@ -3,7 +3,11 @@ import PropTypes from 'prop-types';
 import './ProjectItem.scss';
 
 export default function ProjectItem({ name }) {
-  return <div>{name}</div>;
+  return (
+    <div className="item">
+      <span className="itemName">{name}</span>
+    </div>
+  );
 }
 ProjectItem.propTypes = {
   name: PropTypes.string,
--- frontend/src/components/Project/ProjectList/ProjectList.js
+++ frontend/src/components/Project/ProjectList/ProjectList.js
@@ -4,11 +4,24 @@ import './ProjectList.scss';
 
 export default function ProjectList({ items }) {
   return (
-    <div className="tableContainer">
-      {items.map((projectItem) => (
-        <ProjectItem name={projectItem.name} key={`${projectItem.name}key`} />
-      ))}
-    </div>
+    <table className="tableContainer">
+      <thead className="tableHeader">
+        <th className="head">#</th>
+        <tr>
+          <th>Project name</th>
+        </tr>
+      </thead>
+      <tbody className="tableContent">
+        {items.map((projectItem, index) => (
+          <tr className="contentWrapper" key={`${projectItem.name}key`}>
+            <th className="head">{index}</th>
+            <td>
+              <ProjectItem name={projectItem.name} />
+            </td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
   );
 }
 
