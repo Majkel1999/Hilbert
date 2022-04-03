d57beb3ec37b10b50daed76e459e2afce72f7da2
Kacper <kacper.791@o2.pl>
2022-04-03 12:06:19 +0200

    adding project slice reducers

--- frontend/src/store/projects/projects-slice.js
+++ frontend/src/store/projects/projects-slice.js
@@ -1,11 +1,40 @@
 import { createSlice } from '@reduxjs/toolkit';
 
-const initialProjectsState = {};
+const initialProjectsState = {
+  items: [],
+  changed: false,
+  totalQuantity: 0,
+};
 
 const projectsSlice = createSlice({
   name: 'projects',
   initialState: initialProjectsState,
-  reducers: {},
+  reducers: {
+    createNewProject(state, action) {
+      const newProject = action.payload;
+      const existingProject = state.items.find(
+        (item) => item.id === newProject.id,
+      );
+      state.totalQuantity++;
+      state.changed++;
+      if (!existingProject) {
+        state.items.push({
+          id: newProject.id,
+          quantity: 1,
+          name: newProject.title,
+        });
+      }
+    },
+    removeProject(state, action) {
+      const id = action.payload;
+      const projectToRemoveIndex = state.items.findIndex(
+        (item) => item.id === id,
+      );
+      state.totalQuantity--;
+      state.changed = true;
+      if (projectToRemoveIndex) state.items.splice(projectToRemoveIndex, 1);
+    },
+  },
 });
 
 export const projectsActions = projectsSlice.actions;
