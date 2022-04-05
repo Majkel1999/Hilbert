44aebb69e463fdc20cd50c60c6deaa53fcd047eb
Kacper <kacper.791@o2.pl>
2022-04-05 14:49:05 +0200

    deleting not used mocked projects data

--- frontend/src/pages/AdminBoard/AdminBoard.js
+++ frontend/src/pages/AdminBoard/AdminBoard.js
@@ -4,25 +4,6 @@ import { useSelector, useDispatch } from 'react-redux';
 import * as projectActions from '../../store/projects/project-actions';
 import ProjectList from '../../components/Project/ProjectList/ProjectList';
 
-// eslint-disable-next-line no-unused-vars
-const MOCKED_PROJECTS = [
-  {
-    name: 'Project 1',
-  },
-  {
-    name: 'Project 2',
-  },
-  {
-    name: 'Project 3',
-  },
-  {
-    name: 'Project 4',
-  },
-  {
-    name: 'Project 5',
-  },
-];
-
 export default function AdminBoard() {
   const dispatch = useDispatch();
   const projects = useSelector((state) => state.projects);
