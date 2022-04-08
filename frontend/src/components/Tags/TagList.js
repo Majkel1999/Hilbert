8a77f021d5eba0d198c99c1e1eb59f6b82a78200
Kacper <kacper.791@o2.pl>
2022-04-09 00:34:43 +0200

    creating uuid func and using it

--- frontend/src/components/Tags/TagList.js
+++ frontend/src/components/Tags/TagList.js
@@ -1,13 +1,22 @@
 import PropTypes from 'prop-types';
 import './TagList.scss';
+import { uuid } from '../../utils/utils';
 import Chip from '../UI/Chip/Chip';
 
 export default function TagList({ tags }) {
+  const addNewTag = () => {
+    console.log('newTag');
+  };
   return (
     <div className="tagList">
       {tags &&
-        tags.map((tag) => <Chip chipText={tag} displayDeleteIcon="true" />)}
-      <Chip chipText={<i className="fa fa-plus" aria-hidden="true" />} />
+        tags.map((tag) => (
+          <Chip chipText={tag} key={uuid()} displayDeleteIcon />
+        ))}
+      <Chip
+        chipText={<i className="fa fa-plus" aria-hidden="true" />}
+        onClick={addNewTag}
+      />
     </div>
   );
 }
--- frontend/src/utils/utils.js
+++ frontend/src/utils/utils.js
@@ -1,3 +1,12 @@
+/* eslint-disable no-bitwise */
 // eslint-disable-next-line import/prefer-default-export
 export const capitalizeFirstLetter = (string) =>
   string.charAt(0).toUpperCase() + string.slice(1);
+
+export const uuid = () =>
+  ([1e7] + -1e3 + -4e3 + -8e3 + -1e11).replace(/[018]/g, (c) =>
+    (
+      c ^
+      (crypto.getRandomValues(new Uint8Array(1))[0] & (15 >> (c / 4)))
+    ).toString(16),
+  );
