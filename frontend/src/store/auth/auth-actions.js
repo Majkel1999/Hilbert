0e5b52be6bdc20e67ebdd995de58bb983515b693
Michał Kaleta <mkaleta1999@gmail.com>
2022-04-11 14:08:00 +0200

    Added refresh token action to auth-actions
    Created new axios instance for auth actions without interceptors

--- frontend/src/store/auth/auth-actions.js
+++ frontend/src/store/auth/auth-actions.js
@@ -1,12 +1,19 @@
-import axios from '../../api/axios';
+import axios from 'axios';
 import { authActions } from './auth-slice';
 
+const BASE_URL = process.env.REACT_APP_SERVER_URL;
 const LOGIN_URL = '/user/login';
+const REFRESH_URL = 'user/refresh';
 const REGISTER_URL = '/user/register';
+
+const instance = axios.create({
+  baseURL: BASE_URL,
+});
+
 
 export const login = (loginData) => async (dispatch) => {
   try {
-    const response = await axios.post(LOGIN_URL, loginData);
+    const response = await instance.post(LOGIN_URL, loginData);
 
     dispatch(
       authActions.login({
@@ -18,9 +25,29 @@ export const login = (loginData) => async (dispatch) => {
   }
 };
 
+export const refresh = () => async (dispatch) => {
+  try {
+    const token = JSON.parse(localStorage.getItem('refresh_token'));
+    const response = await instance.post(REFRESH_URL, null,
+      {
+        headers: {
+          'Authorization': token
+            ? `${token.token_type} ${token.refresh_token}`
+            : ''
+        }
+      });
+    dispatch(authActions.login({
+      token: response.data
+    }))
+  }
+  catch (error) {
+    console.log(error);
+  }
+}
+
 export const register = (registerData) => async () => {
   try {
-    const response = await axios.post(REGISTER_URL, registerData);
+    const response = await instance.post(REGISTER_URL, registerData);
     return response;
   } catch (error) {
     console.log(error);
@@ -31,4 +58,5 @@ export const register = (registerData) => async () => {
 export default {
   login,
   register,
+  refresh
 };
--- frontend/src/store/auth/auth-slice.js
+++ frontend/src/store/auth/auth-slice.js
@@ -10,11 +10,15 @@ const authSlice = createSlice({
   reducers: {
     login(state, action) {
       const receivedToken = action.payload.token;
-      localStorage.setItem('token', JSON.stringify(receivedToken));
+      localStorage.setItem('token', JSON.stringify({ "token_type": receivedToken.token_type, "access_token": receivedToken.access_token }));
+      if (receivedToken.refresh_token) {
+        localStorage.setItem('refresh_token', JSON.stringify({ "token_type": receivedToken.token_type, "refresh_token": receivedToken.refresh_token }));
+      }
       state.isLoggedIn = true;
     },
     logout(state) {
       localStorage.removeItem('token');
+      localStorage.removeItem('refresh_token');
       state.isLoggedIn = false;
     },
   },
