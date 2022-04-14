937e02cc05a6b3aba4d42e415134f696970a7cb5
Kacper <kacper.791@o2.pl>
2022-04-14 21:23:24 +0200

    changing openProject page styles

--- frontend/src/pages/OpenedProject/OpenedProject.js
+++ frontend/src/pages/OpenedProject/OpenedProject.js
@@ -36,18 +36,6 @@ export default function OpenedProject() {
 
   return (
     <div className="openedProjectContainer">
-      <div className="inviteUrlWrapper">
-        <h2> {currentProjectData.inviteUrl} </h2>
-        <i
-          className="fa fa-copy"
-          aria-hidden="true"
-          onClick={() => {
-            navigator.clipboard.writeText(
-              `${process.env.REACT_APP_WEB_URL}/${currentProjectData.inviteUrl}/train`,
-            );
-          }}
-        />
-      </div>
       <div className="textOperationsWrapper">
         <TagList
           tags={currentProjectData.tags}
@@ -56,6 +44,18 @@ export default function OpenedProject() {
           displayDeleteIcon={false}
         />
         <div className="textContainer">
+          <div className="inviteUrlWrapper">
+            <h2> {currentProjectData.inviteUrl} </h2>
+            <i
+              className="fa fa-copy"
+              aria-hidden="true"
+              onClick={() => {
+                navigator.clipboard.writeText(
+                  `${process.env.REACT_APP_WEB_URL}/${currentProjectData.inviteUrl}/train`,
+                );
+              }}
+            />
+          </div>
           <div className="textWrapper" />
           <Button text="Train model" />
         </div>
