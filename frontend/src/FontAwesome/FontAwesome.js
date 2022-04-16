d8a32bfed6f21d780583e265a66deb51a64cf7e3
Kacper <kacper.791@o2.pl>
2022-04-16 19:01:06 +0200

    adding needed icon

--- frontend/src/FontAwesome/FontAwesome.js
+++ frontend/src/FontAwesome/FontAwesome.js
@@ -3,9 +3,18 @@ import {
   faMinus,
   faPlusCircle,
   faTrashAlt,
+  faXmark,
   faXmarkCircle,
 } from '@fortawesome/free-solid-svg-icons';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { fab } from '@fortawesome/free-brands-svg-icons';
 
-library.add(fab, faTrashAlt, faMinus, faCopy, faXmarkCircle, faPlusCircle);
+library.add(
+  fab,
+  faTrashAlt,
+  faMinus,
+  faCopy,
+  faXmarkCircle,
+  faXmark,
+  faPlusCircle,
+);
