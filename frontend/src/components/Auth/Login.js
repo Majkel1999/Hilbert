2e76c5b74c2230eaf3715a8f2ca0de163f35cbf2
krzysiek9 <wrzysztofk999@gmail.com>
2022-05-08 20:32:22 +0200

    home, login, register - styles

--- frontend/src/components/Auth/Login.js
+++ frontend/src/components/Auth/Login.js
@@ -1,27 +1,28 @@
-import { useEffect, useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { useNavigate, useLocation, Link } from 'react-router-dom';
-import { login } from '../../store/auth/auth-actions';
-import GenericForm from '../GenericForm/GenericForm';
-import * as routes from '../../constants/routes';
+import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate, useLocation, Link } from "react-router-dom";
+import { login } from "../../store/auth/auth-actions";
+import GenericForm from "../GenericForm/GenericForm";
+import * as routes from "../../constants/routes";
+import "./Login.scss";
 
 export default function Login() {
   const isLoggedIn = useSelector((state) => state.auth.isLoggedIn);
   const dispatch = useDispatch();
   const navigate = useNavigate();
   const location = useLocation();
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
   const inputArray = [
     {
-      label: 'username',
-      inputType: 'text',
+      label: "username",
+      inputType: "text",
       setValue: setUsername,
       inputValue: username,
     },
     {
-      label: 'password',
-      inputType: 'password',
+      label: "password",
+      inputType: "password",
       setValue: setPassword,
       inputValue: password,
     },
@@ -32,8 +33,8 @@ export default function Login() {
   const loginHandler = (e) => {
     e.preventDefault();
     const loginFormData = new FormData();
-    loginFormData.set('username', username);
-    loginFormData.set('password', password);
+    loginFormData.set("username", username);
+    loginFormData.set("password", password);
     dispatch(login(loginFormData));
   };
 
@@ -48,7 +49,11 @@ export default function Login() {
         onSubmitHandler={loginHandler}
         formInputArray={inputArray}
         buttonText="Login"
-        redirectComponent={<Link to={routes.REGISTER}>Create new account</Link>}
+        redirectComponent={
+          <Link to={routes.REGISTER} className="newAccount">
+            Create new account
+          </Link>
+        }
       />
     </div>
   );
---
+++ frontend/src/components/UI/SnackBar/SnackBar.js
@@ -0,0 +1,32 @@
+import React, { useState, forwardRef, useImperativeHandle } from "react";
+import "./SnackBar.scss";
+import DoneIcon from "@mui/icons-material/Done";
+import ClearIcon from "@mui/icons-material/Clear";
+
+const SnackBar = forwardRef((props, ref) => {
+  const [showSnackbar, setShowSnackbar] = useState(true);
+
+  useImperativeHandle(ref, () => ({
+    show() {
+      setShowSnackbar(true);
+      setTimeout(() => {
+        setShowSnackbar(false);
+      }, 3000);
+    },
+  }));
+
+  return (
+    <div className="snackbar" id={showSnackbar ? "show" : "hide"}>
+      <div className="symbol">
+        {props.type === "success" ? (
+          <DoneIcon color="success" fontSize="large" />
+        ) : (
+          <ClearIcon color="warning" fontSize="large" />
+        )}
+      </div>
+      <div className="message">{props.message}</div>
+    </div>
+  );
+});
+
+export default SnackBar;
