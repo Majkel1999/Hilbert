7024575b844dc25e1f3e74805e4b86ec404e6a87
Michał Kaleta <mkaleta1999@gmail.com>
2022-05-09 12:31:53 +0200

    First csv iteration

--- backend/app/utility/file_helper.py
+++ backend/app/utility/file_helper.py
@@ -1,26 +1,28 @@
 import re
+from venv import create
 import zipfile
-from io import BytesIO
+import csv
+from io import BytesIO, StringIO
 from os import path
-from typing import List
+from typing import List, final
 
-from app.models.project_models import TextDocument
+# from app.models.project_models import TextDocument
 from fastapi import HTTPException, UploadFile, status
 from pdfminer.high_level import extract_text
 
 
-def createDocument(content: str, name: str) -> TextDocument:
-    text = TextDocument(name=name, value=content)
+def createDocument(content: str, name: str, tag: List[str] = None):
+    text = {"name": name, "value": content, "tags" : tag}
     return text
 
 
-def handlePdf(file: bytes, name: str) -> TextDocument:
+def handlePdf(file: bytes, name: str):
     content = extract_text(BytesIO(file))
     content = re.sub('\s\s+', ' ', content)
     return createDocument(content, name)
 
 
-async def handleZip(file: bytes) -> List[TextDocument]:
+async def handleZip(file: bytes):
     result = list()
     fileIo = BytesIO(file)
     with zipfile.ZipFile(fileIo) as archive:
@@ -31,7 +33,53 @@ async def handleZip(file: bytes) -> List[TextDocument]:
     return result
 
 
-async def checkExtensios(file: bytes, name: str, extension: str) -> List[TextDocument]:
+def handleCsv(file: bytes, fileName: str):
+    file = StringIO(file.decode())
+    reader = csv.reader(file, delimiter=';')
+    header = next(reader)
+    header = [text.casefold().strip() for text in header]
+    textIndex = findIndex(header, "text")
+    nameIndex = findIndex(header, "name", False)
+    tagIndex = findIndex(header, "tag", False)
+    texts = []
+    idx = 1
+    for row in reader:
+        print(row)
+        tag = []
+        name = f'{fileName}_{idx}'
+        text = row[textIndex]
+        if(tagIndex != -1):
+            try:
+                tag = row[tagIndex].split(',')
+            except:
+                pass
+        if(nameIndex != -1):
+            try:
+                name = row[nameIndex]
+            except:
+                pass
+        idx = idx + 1
+        texts.append(createDocument(text,name,[value.casefold().strip() for value in tag]))
+    return texts
+
+
+def findIndex(list: List[str], header: str, throw: bool = True) -> int:
+    found = True
+    index = -1
+    try:
+        index = list.index(header)
+    except ValueError:
+        found = False
+    finally:
+        if(not found and throw):
+            raise HTTPException(
+                status_code=status.HTTP_400_BAD_REQUEST,
+                detail=f'Header: "{header}" not found in csv file'
+            )
+    return index
+
+
+async def checkExtensios(file: bytes, name: str, extension: str):
     result = list()
     if(extension == ".txt"):
         result.append(createDocument(file, name))
@@ -39,6 +87,8 @@ async def checkExtensios(file: bytes, name: str, extension: str) -> List[TextDoc
         result.append(handlePdf(file, name))
     elif(extension == ".zip"):
         result.extend(await handleZip(file))
+    elif(extension == ".csv"):
+        result.extend(handleCsv(file))
     else:
         raise HTTPException(
             status_code=status.HTTP_400_BAD_REQUEST,
@@ -47,7 +97,10 @@ async def checkExtensios(file: bytes, name: str, extension: str) -> List[TextDoc
     return result
 
 
-async def handleFile(file: UploadFile) -> List[TextDocument]:
+async def handleFile(file: UploadFile):
     name, file_ext = path.splitext(file.filename)
     content = await file.read()
     return await checkExtensios(content, name, file_ext)
+
+
+print(handleCsv(b'text;tag \n"tekst jest; taki";1 \n Taki tez jest;123l,123;0', "dupa"))
