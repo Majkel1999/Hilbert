3ecbeb18c1334434b4a9e1a119e010e9ea1a6d89
Kacper <kacper.791@o2.pl>
2022-05-09 21:25:58 +0200

    adding color depending on snackBar type

--- frontend/src/components/UI/SnackBar/SnackBar.js
+++ frontend/src/components/UI/SnackBar/SnackBar.js
@@ -5,7 +5,7 @@ import DoneIcon from '@mui/icons-material/Done';
 import ClearIcon from '@mui/icons-material/Clear';
 
 const SnackBar = ({ type, show, message }) => (
-  <div className="snackbar" id={show ? 'show' : 'hide'}>
+  <div className={`snackbar ${type}`} id={show ? 'show' : 'hide'}>
     <div className="symbol">
       {type === 'success' ? (
         <DoneIcon color="success" fontSize="large" />
