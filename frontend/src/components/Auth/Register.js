d833d23a578b1c88d0100b12eba9e83de4f335bf
Kacper <kacper.791@o2.pl>
2022-05-10 13:33:44 +0200

    navigate to login when registration successfull

--- frontend/src/components/Auth/Register.js
+++ frontend/src/components/Auth/Register.js
@@ -1,26 +1,26 @@
-import { useState } from "react";
-import { useNavigate, Link } from "react-router-dom";
-import { useDispatch } from "react-redux";
-import { register } from "../../store/auth/auth-actions";
-import GenericForm from "../GenericForm/GenericForm";
-import * as routes from "../../constants/routes";
-import "./Register.scss";
+import { useState } from 'react';
+import { useNavigate, Link } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import { register } from '../../store/auth/auth-actions';
+import GenericForm from '../GenericForm/GenericForm';
+import * as routes from '../../constants/routes';
+import './Register.scss';
 
 export default function Register() {
   const dispatch = useDispatch();
   const navigate = useNavigate();
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
   const inputArray = [
     {
-      label: "username",
-      inputType: "text",
+      label: 'username',
+      inputType: 'text',
       setValue: setUsername,
       inputValue: username,
     },
     {
-      label: "password",
-      inputType: "password",
+      label: 'password',
+      inputType: 'password',
       setValue: setPassword,
       inputValue: password,
     },
@@ -29,11 +29,11 @@ export default function Register() {
   const registerHandler = (e) => {
     e.preventDefault();
     const registerFormData = new FormData();
-    registerFormData.set("username", username);
-    registerFormData.set("password", password);
+    registerFormData.set('username', username);
+    registerFormData.set('password', password);
     const responsePromise = dispatch(register(registerFormData));
     responsePromise.then((response) => {
-      if (response.status === 201) navigate(routes.LOGIN, { replace: true }); // need to fix
+      if (response.status === 200) navigate(routes.LOGIN, { replace: true }); // need to fix
     });
   };
 
