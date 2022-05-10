b110bb32dab62c1a07385a043cd01da035a9c972
Michał Kaleta <mkaleta1999@gmail.com>
2022-05-10 07:59:55 +0200

    Remove MLModel imports

--- backend/app/utility/connectors/database_connector.py
+++ backend/app/utility/connectors/database_connector.py
@@ -3,7 +3,7 @@ import os
 from beanie import init_beanie
 from motor.motor_asyncio import AsyncIOMotorClient
 
-from app.models.project_models import MLModel, Project, TextDocument
+from app.models.project_models import Project, TextDocument
 from app.models.user_models import User
 
 
@@ -22,7 +22,7 @@ db = Database()
 async def init_db():
     print("Initializing database connection")
     db.client = AsyncIOMotorClient(DB_CONN_STRING)
-    await init_beanie(db.client.test, document_models=[User, Project, TextDocument, MLModel])
+    await init_beanie(db.client.test, document_models=[User, Project, TextDocument])
     print("Database connection initialized")
 
 
