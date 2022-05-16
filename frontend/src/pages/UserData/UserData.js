8c17fe891fe1a698b7d25885184effdbf3b9d837
Kacper <kacper.791@o2.pl>
2022-05-17 01:35:40 +0200

    showing user Data

--- frontend/src/pages/UserData/UserData.js
+++ frontend/src/pages/UserData/UserData.js
@@ -3,6 +3,7 @@ import './UserData.scss';
 import { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { getUserDetails, deleteUser } from '../../store/user/user-actions';
+import Button from '../../components/UI/Button/Button';
 
 export default function UserData() {
   const dispatch = useDispatch();
@@ -14,7 +15,24 @@ export default function UserData() {
 
   return (
     <div className="userInfoWrapper">
-      <div className="header"> </div>
+      <div className="header">
+        <h3>User Data</h3>
+      </div>
+      <div className="userDataContainer">
+        <div className="userDataItem">
+          <label>Username:</label>
+          <spam>{userData.username || 'Unknown'}</spam>
+        </div>
+        <div className="userDataItem">
+          <label>Full name:</label>
+          <spam>{userData.fullName || 'Unknown'}</spam>
+        </div>
+        <div className="userDataItem">
+          <label>Email:</label>
+          <spam>{userData.email || 'Unknown'}</spam>
+        </div>
+      </div>
+      <Button text="Delete user" onClickHandler={deleteUser} />
     </div>
   );
 }
